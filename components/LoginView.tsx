3fc574b246def8a735db4beec880e67872c28c6b
Giovanni Gioia Carrozzino <carrozzino201@gmail.com>
2025-11-27 18:07:56 -0300

    Fix: Add correct image files with proper names

--- components/LoginView.tsx
+++ components/LoginView.tsx
@@ -32,7 +32,7 @@ const LoginView: React.FC<LoginViewProps> = ({ onLogin }) => {
             </div>
           ) : (
             <img 
-              src="./assets/logo.png" 
+              src="./assets/Logo.png" 
               alt="LevelDay Logo" 
               className="w-32 h-32 object-contain"
               onError={() => setLogoError(true)}
